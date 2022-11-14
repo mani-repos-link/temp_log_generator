26a8440622fe65dc4c41665133684f09e93c3127
Manpreet <mansingh@eurac.edu>
2022-11-14 16:32:24 +0100

    exporting wrong xes files?

--- log_generator/ASP_generator.py
+++ log_generator/ASP_generator.py
@@ -65,51 +65,24 @@ class ASP_generator(Log_generator):
                         if num not in traced:
                             traced[num] = {}
                         traced[num][trace_name] = l
+
         print(traced)
         lines = []
         for trace_id in range(len(traced)):
-            trace_gen = lg.Trace()
-            line = { 'case_id': trace_id}
-            trace_gen.attributes["concept:name"] = f"trace_{trace_id}"
+            line = {'case_id': trace_id}
             for i in traced:
                 trace = traced[i]
                 event_name = trace["trace"][0]
-                line['concept:name'] = event_name
-                # trace_gen.attributes["concept:name"] =    # todo: its event
+                line['case:concept:name'] = event_name
                 e = {i: trace[i] for i in trace if i != 'trace'}  # filter trace by removing trace key
                 for i in e:  # e = {'assigned_value': ['grade', '5', '1']}
-                    line["resource"] = e[i][0]
-                    line["cost"] = e[i][1]
-                    line["key"] = i
-                    event = lg.Event()
-                    event["concept:name"] = event_name  # trace[i]  ## trace["trace"][0]
-                    event[e[i][0]] = e[i][1]  ## trace["trace"][0]
-                    event["time:timestamp"] = datetime.now().timestamp()  # + timedelta(hours=c).datetime
+                    line['concept:name'] = e[i][0]
                     line["time:timestamp"] = datetime.now().timestamp()  # + timedelta(hours=c).datetime
-                    trace_gen.append(event)
             lines.append(line)
-            self.trace_xes.append(trace_gen)
-                #     trace_xes.append(event)
 
         dt = pd.DataFrame(lines)
-        print(dt)
-        dt = pm4py.format_dataframe(dt, case_id='case_id', activity_key='resource',
+        dt = pm4py.format_dataframe(dt, case_id='case_id', activity_key='concept:name',
                                            timestamp_key='time:timestamp')
-
         logger = pm4py.convert_to_event_log(dt)
         pm4py.write_xes(logger, 'logy.xes')
-        # d = exporter.log_conversion.to_data_frame.apply(self.trace_xes)
-        # print("Traces length")
-        print(logger)
-        # i = 0
-        # for p in self.trace_xes:
-        #     i = i + 1
-        #     ev.__dict__[i] = p
 
-        # exporter.apply(ev, "logx.xes")
-        # print(m[0].type, m[0]._rep, m[0].name, m[0].arguments, m[0].negative, m[0].number, m[0].positive)
-        # print(m[0].string, m[0].type, m[0]._rep, m[0].name, m[0].arguments, m[0].negative, m[0].number, m[0].positive)
-
-        # for c in output.context.symbolic_atoms:
-        # print(c.symbol, "\t\t", c.is_external, "\t\t", c.is_fact, "\t\t", c.literal, "\t", c.__str__())
-        # df = pm4py.format_dataframe()
